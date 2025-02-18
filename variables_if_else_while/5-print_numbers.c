6620cc9db293e946663ce49a3322172e1f4e07a6
root <shakibrojas@icloud.com>
2025-02-18 15:47:30 +0000

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*liste de 0 a 19*/
+/*liste de 0 a 9*/
 	int main(void)
 {
 	int b10 = 48;
 
 	while (b10 < 58)
 {
-	putchar(b10);
+	putchar(b10\n);
 	if (b10 < 57)
-
+}
 	b10++;
 }
-	putchar('\n');
 return (0);
 }
