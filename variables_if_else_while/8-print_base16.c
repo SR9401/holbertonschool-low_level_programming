a155337db0eac1ed68be5314325cf73a1174d0e9
root <shakibrojas@icloud.com>
2025-02-19 08:57:39 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+{
+	int b10 = 48;
+	int af = 65;
+
+	while (b10 < 58)
+
+{
+	putchar(b10);
+
+	b10++;
+}
+	while (af < 71)
+{
+	putchar(af);
+
+	af++;
+}
+{
+	putchar('\n');
+}
+return (0);
+}
