fdfb01f9d198cda381cc0b9a23d07dc94a0e9cad
root <shakibrojas@icloud.com>
2025-02-19 08:41:53 +0000

    8

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alphabet[] = "zyxwvutsrqponmlkjihgfedcba\n";
+        int i = 0;
+
+        while (alphabet[i])
+{
+        putchar(alphabet[i]);
+        i++;
+}
+return (0);
+}
