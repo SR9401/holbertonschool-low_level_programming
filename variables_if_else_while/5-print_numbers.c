6d93ce2963fd18099c3c8316de151c8488ad460e
root <shakibrojas@icloud.com>
2025-02-19 08:32:15 +0000

    7

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*liste de 0 a 9*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 	int main(void)
 {
 	int b10 = 48;
@@ -9,9 +13,12 @@
 	while (b10 < 58)
 {
 	putchar(b10);
-	if (b10 < 58)
+	/*if (b10 < 58)*/
 
 	b10++;
+}
+{
+	putchar('\n');
 }
 return (0);
 }
