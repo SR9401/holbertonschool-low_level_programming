3d90c8056cd58be0df82246612f4abc408a08e1c
root <shakibrojas@icloud.com>
2025-02-19 08:35:39 +0000

    9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+{
+	int b10 = 48;
+
+	while (b10 < 58)
+{
+	putchar(b10);
+	/*if (b10 < 58)*/
+
+	b10++;
+}
+{
+	putchar('\n');
+}
+return (0);
+}
