8c5e318ff73b10a7aae2abfd36a0b64fa2815199
root <shakibrojas@icloud.com>
2025-02-19 09:30:11 +0000

    7

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+{
+/*	int b10 = 48;
+
+	int space = 27;
+
+	int vir = 44;
+*/
+	int  b10 = 48;
+	while (b10 < 57)
+
+{
+	putchar(b10);
+
+	putchar(',');
+
+	putchar(' ');
+
+	b10++;
+}
+
+	putchar('9');
+
+return (0);
+}
