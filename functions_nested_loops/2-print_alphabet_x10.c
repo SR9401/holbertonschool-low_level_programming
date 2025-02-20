20451e91e59a97a99104b0984414152e0bdaea95
root <shakibrojas@icloud.com>
2025-02-20 15:39:53 +0000

    8

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - mm
+ *
+ *  Return: Always 0
+*/
+
+int print_alphabet_x10();
+
+	int a = 0;
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 int print_alphabet();
+int print_alphabet_x10();
