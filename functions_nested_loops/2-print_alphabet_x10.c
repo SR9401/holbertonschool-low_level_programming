8a753df4906a0a0688304d2cff49c6ea46b3e6ea
root <shakibrojas@icloud.com>
2025-02-21 08:27:50 +0000

    8

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,25 @@
  *  Return: Always 0
 */
 
-int print_alphabet_x10();
+int print_alphabet_x10(void)
 
+{
 	int a = 0;
 
+	int alpha = 'a';
+
+	while (a < 10)
+{
+	while (alpha < 'z')
+{
+	_putchar(alpha);
+
+	_putchar('\n');
+
+	a++;
+
+	alpha++;
+}
+}
+	return(0);
+}
