7787fc4cae61065b0fcbc1f9e8e0734e16f71430
root <shakibrojas@icloud.com>
2025-02-21 10:15:43 +0000

    4

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - mm
+ *
+ *  Return: Always 0
+*/
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+
+	return (1);
+
+	else
+
+	return (0);
+}
