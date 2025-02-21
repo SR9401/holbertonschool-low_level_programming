3b388a8c8a46a982dbfe1bda260e779f647cafaa
root <shakibrojas@icloud.com>
2025-02-21 14:07:56 +0000

    8

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *print_last_digit - mm
+ *@n:  mmm
+ *  Return: Always 0
+*/
+
+int print_last_digit(int n)
+{
+	putchar(abs(n % 10));
+
+	return (abs(n % 10));
+}
