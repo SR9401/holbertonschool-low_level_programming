18b2418bf80a45178b237c0b5cdceb4789cb4c25
root <shakibrojas@icloud.com>
2025-02-23 07:55:38 +0000

    9

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *add - mm
+ *@n1: lklk
+ *@n2: pol
+ *  Return: Always 0
+*/
+
+int add(int n1, int  n2)
+{
+
+	return (n1 + n2);
+
+}
+
