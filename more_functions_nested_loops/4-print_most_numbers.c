dfd180ed0a55f90213c374f4ce6ef5452c22edda
root <shakibrojas@icloud.com>
2025-02-24 10:45:16 +0000

    pmn

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *print_most_numbers - mm
+ *
+ *  Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+	int b10 = 48;
+
+	while (b10 < 58)
+{
+	if (b10 == 50 ||b10 == 52)
+
+{
+	b10++;
+
+}
+	_putchar(b10);
+
+	b10++;
+}
+
+	_putchar('\n');
+}
