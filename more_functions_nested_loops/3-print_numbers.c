9e74828907feb23e745c4a1ab7a73ab6e19935dd
root <shakibrojas@icloud.com>
2025-02-24 10:32:17 +0000

    5

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *print_numbers - mm
+ *
+ *  Return: Always 0
+*/
+
+void print_numbers(void)
+{
+	int b10 = 48;
+
+	while (b10 < 58)
+{
+	putchar(b10);
+
+	b10++;
+}
+
+	putchar('\n');
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -11,3 +11,5 @@ int add(int, int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
