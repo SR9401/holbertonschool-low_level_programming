0e54dfc59e608277feb42e504a3178433341c4a0
root <shakibrojas@icloud.com>
2025-02-25 11:19:48 +0000

    7

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+
+{
+	int num = 0
+
+
+		while (num < 100)
+	{
+			if (num % 3 = 0)
+		{
+			printf("Fizz")
+			num++;
+		}
+			else if (num % 5 = 0)
+		{
+			printf("Buzz")
+			num++
+		}
+			else
+		{
+			_putchar(num);
+			num++;
+		}
+
+	}
+
+}
