031def17ff81dedf89a57d42d1b7c62202a0a048
root <shakibrojas@icloud.com>
2025-02-25 08:55:31 +0000

    7

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,36 @@
+#include "main.h"
+ #include <stdio.h>
+/**
+ * print_line - mm
+ *
+ *  Return: Always 0
+ *
+ * @n: rrr
+ *
+*/
+
+
+void print_line(int n)
+
+{
+
+	int a = 0;
+
+		if (n > 0)
+
+	{
+			while (a < n)
+		{
+			_putchar('_');
+			a++;
+		}
+
+		_putchar('\n');
+	}
+		else
+	{
+		_putchar('\n');
+	}
+
+	}
+
