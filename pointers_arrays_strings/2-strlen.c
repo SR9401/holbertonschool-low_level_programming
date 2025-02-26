bf168e60ebe03510b99ac6fecf705bbf7f073950
root <shakibrojas@icloud.com>
2025-02-26 10:43:39 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strlen - pppp
+*
+*@*s: rrr
+*Return: lenght of *s
+**/
+
+	int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+{
+	l++;
+	s++;
+}
+	return(l);
+}
