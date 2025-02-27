67c88de3522113d9a8f40443374ac9aa53e8f7c0
root <shakibrojas@icloud.com>
2025-02-27 20:56:16 +0000

    9

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*puts2 - pppp
+*
+*@str: rrr
+*Return: Always 0
+**/
+
+
+
+	void puts2(char *str)
+
+{
+
+
+		while (*str != '\0')
+	{
+
+		_putchar(*str);
+		str++;
+		str++;
+
+
+	}
+
+	_putchar('\n');
+}
