ece1db66e2979edf047e98ac4cfc00bb37559e00
root <shakibrojas@icloud.com>
2025-03-04 11:34:16 +0000

    8

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strstr - pppp
+*
+*@haystack: yyyy
+*
+*@needle: rrr
+*Return: Always 0
+**/
+
+/*char *_strstr(char *haystack, char *needle)
+{
+	int i, b;
+	i = 0;
+		while (haystack[i] != '\0')
+	{
+		b = 0;
+			while (needle[b] != '\0')
+		{
+			if (needle[b] == haystack[i])
+			int c = i;
+			b++;
+			i++;
+        {
+		}
+		i++;
+	}
+	return (NULL);
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, b;
+	b = 0;
+		while (needle[i] != haystack[b])
+	{
+		i = 0;
+		b++;
+	}
+		if (needle[i] == haystack[b])
+	{
+			while (haystack[b] < '\0' || needle[i] != haystack[b])
+		{
+			i++;
+			b++;
+				if (needle[i] == '\0')
+			{
+				return (needle + 0);
+			}
+				else
+			{
+				return (NULL);
+			}
+		}
+	}
+return (NULL);
+}
