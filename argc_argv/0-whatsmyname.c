19ac0feee85a93562e496b8848b6d925930669c1
root <shakibrojas@icloud.com>
2025-03-06 16:01:07 +0000

    p

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+*main - jjjjj
+*
+*@argc: ppppp
+*
+*@argv: 45452
+*
+*Return: zero
+**/
 
-	int main(int argc __attribute__((unused)),char *argv[])
+
+	int main(int argc __attribute__((unused)), char *argv[])
 {
 
 	printf("%s\n", argv[0]);
