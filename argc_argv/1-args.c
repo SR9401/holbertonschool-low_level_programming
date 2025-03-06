23609709864f6f95351df74edb9d808cf944cf33
root <shakibrojas@icloud.com>
2025-03-06 16:19:24 +0000

    r

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - jjjjj
+*
+*@argc: ppppp
+*
+*@argv: 45452
+*
+*Return: zero
+**/
+
+
+	int main(int argc, char *argv[]__attribute__((unused)))
+{
+
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
