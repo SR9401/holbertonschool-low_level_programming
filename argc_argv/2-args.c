9329f7200dbb2b2c5f2dbb6d716aaa833db5a11d
root <shakibrojas@icloud.com>
2025-03-07 10:08:55 +0000

    i

---
+++ argc_argv/2-args.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - jjjjj
+*
+*@argc: ppppp
+*
+*@argv: 45452
+*
+*Return: zero
+**/
+
+
+
+	int main(int argc, char *argv[])
+
+{
+
+int a = 0;
+	while (a < argc)
+	{
+
+	printf("%s\n", argv[a]);
+	a++;
+	}
+
+return (0);
+}
