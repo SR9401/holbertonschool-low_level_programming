52b25be1777b6ea7ce569827dfb0c7a5a585cd2b
root <shakibrojas@icloud.com>
2025-03-11 13:50:22 +0000

    p

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *malloc_checked - prints str in s
+ *@b: fer
+ *
+ *
+ * Return: Nothing.
+ */
+
+	void *malloc_checked(unsigned int b)
+
+{
+	unsigned int *s;
+	s = malloc(b);
+	if (s == NULL)
+	{
+		exit(98);
+	}
+	return (s);
+}
