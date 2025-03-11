0517e00c5357d88419ecc1b956c301a6c8d0ede2
root <shakibrojas@icloud.com>
2025-03-11 14:26:45 +0000

    u

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *string_nconcat - prints str in s
+ * @s1: fer
+ *@s2: rlr
+ *@n: pplpl
+ * Return: Nothing.
+ */
+
+	char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+	char *s;
+	unsigned int i, z;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	for (z = 0; s2[z] != '\0'; z++)
+	if (s1 == NULL)
+	{
+		s2 = "";
+	}
+	s = malloc(((i + z) + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i] = s1[i];
+	}
+	if (n >= z)
+	{
+		for (z = 0; s2[z] != '\0'; z++)
+		{
+			s[i] = s2[z];
+			i++;
+		}
+	}
+	else
+		for (z = 0; z <= n; z++)
+		{
+			s[i] = s2[z];
+			i++;
+		}
+
+	return (s);
+}
