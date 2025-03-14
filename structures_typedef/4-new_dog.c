064cf14eb17241da01d6d0e9ab2816074a76b0f1
unknown <shakibrojas@icloud.com>
2025-03-14 16:06:34 +0100

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - entry point
+ * @name: nom chien
+ * @age: age
+ * @owner: prop
+ * Return: s
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *s;
+	s = malloc(sizeof(dog_t));
+	if (s == NULL)
+		return (NULL);
+
+	if (name == NULL)
+	{
+		free(s);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(s);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (s);
+}
