b36aeb245ee12f4bf24dd92c2b0b377f6b90ff8d
unknown <shakibrojas@icloud.com>
2025-03-14 10:14:00 +0100

    l

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,8 +14,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-    d->name = name;
-    d->age = age
-    d->owner = owner
-    return (0);
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age
+        d->owner = owner
+    }
 }
