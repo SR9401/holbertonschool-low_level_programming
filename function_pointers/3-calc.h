7d081203f5268ff2045634c91a13bc878d61b468
unknown <shakibrojas@icloud.com>
2025-03-19 10:39:54 +0100

    3main

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,3 +1,7 @@
+#ifndef _CALC_
+#define _CALC_
+
+
 /**
  * struct op - Struct op
  *
@@ -14,3 +18,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
