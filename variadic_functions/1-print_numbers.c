8348d88ecdddd52ebcff615455e5456d164bf9dc
unknown <shakibrojas@icloud.com>
2025-03-20 14:41:33 +0100

    1-p-n test

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Retourne la somme de tous ses paramètres
+ * @n: Nombre d'arguments
+ *
+ * Return: Somme des arguments, ou 0 si n == 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+
+
+{
+
+const unsigned int i = 0;
+	va_list args;
+
+	if (separator == NULL || n == 0)
+	{
+		printf("\n");
+	}
+	else
+	{
+	va_start(args, n);
+
+			while (i <= n)
+			{
+				int num = va_arg(args, int);
+				printf("%d", num);
+				if (i < n )
+				{
+					printf("%s", separator);
+				}
+				else
+				{
+					printf("\n");
+				}
+				i++;
+			}
+	va_end
+
+}
