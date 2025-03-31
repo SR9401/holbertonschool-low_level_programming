f60807ac30cd339daa47a6b8996b6aead3556877
root <shakibrojas@icloud.com>
2025-03-31 14:23:27 +0000

    correction #define

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_h
+#ifndef _LIST_H_
+#define _LIST_H_
 
 
 #include <stdlib.h>
@@ -21,9 +21,9 @@ int _putchar(char c);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
