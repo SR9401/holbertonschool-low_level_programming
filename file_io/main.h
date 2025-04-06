88da049397641765d0044f1f6cce8147dfaffa4c
root <shakibrojas@icloud.com>
2025-04-06 14:26:34 +0000

    create file

--- file_io/main.h
+++ file_io/main.h
@@ -2,12 +2,15 @@
 #define _MAIN_H
 
 
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 
 
